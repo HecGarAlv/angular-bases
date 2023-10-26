4b6f604b631319d145c151ca2c769b6ee40ad2b6
Hgarcia <hectorgarciaalvarez@gmail.com>
2023-10-26 18:30:38 +0100

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age:  number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+      return `${ this.name } - ${ this.age }`;
+  }
+
+  changeHero(): void{
+    if(this.name == 'ironman')
+      this.name='spiderman';
+    else
+      this.name='ironman';
+  }
+
+  changeAge():void{
+    if(this.age == 45)
+      this.age = 23;
+    else
+      this.age = 45;
+  }
+
+  resetForm():void{
+    this.age = 45;
+    this.name = 'ironman';
+  }
+
+}
